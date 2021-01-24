a5b80f305c400680dff4aab98f2f3150c38332f6
janrembold <janrembold@gmx.net>
2021-01-24 21:32:45 +0100

    update dist

---
+++ dist/stencil-components/stencil-components.esm.js
@@ -0,0 +1,1 @@
+import{p as t,b as m}from"./p-0f2f488b.js";(()=>{const m=import.meta.url,o={};return""!==m&&(o.resourcesUrl=new URL(".",m).href),t(o)})().then((t=>m([["p-70807124",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],t)));
